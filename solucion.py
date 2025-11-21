920bb8e0eb7360721c732230439fb63d3c85c08b
Luis0-5 <a225216692@unison.mx>
2025-11-21 20:59:23 +0000

    Validate height in triangulo_simetrico function
    Add validation for positive height in triangulo_simetrico function

--- solucion.py
+++ solucion.py
@@ -3,6 +3,9 @@
 
 def triangulo_simetrico(m: int, s: str) -> str:
     # TODO: validar altura mayor que 0 e imprimir "Error: La altura debe ser un entero positivo" y salir
+    if m <= 0:
+        print("Error: La altura debe de ser un entero positivo")
+        return
 
     # Parte 1: Triangulo creciente
     for i in range(1,m+1):
